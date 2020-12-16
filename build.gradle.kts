7ef99a2fec044839a795a0f3e1ad4bdc73e470e5
Ian Ellis <>
2020-12-16 16:17:59 +1100

    Adding Detekt
    Adding as a seperate gradle file that we simply apply to the main build.gradle - this is simply to allow us to use the groovy syntax provided by the docs. We now have ./gradlew detekt which will run across the whole project and use the detekt-config.xml provided in /detekt. we also have an empty base line incase anyone using this template needs to mass import code. You can then simply generate a base line rather than fixing all your issues. This setup allows CLI integration which is good for CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+apply {
+    from("detekt/detekt.gradle")
+}
+
 buildscript {
     repositories {
         google()

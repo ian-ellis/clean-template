c40cec044c4a70a8da69dbfe3fea14a32ef1ea1b
Ian Ellis <>
2020-12-18 10:10:30 +1100

    Adding coverage to data module
    Excluding entities as they should be immutable data classes without any logic

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,11 +1,14 @@
 import com.github.ianellis.clean.Dependencies
 import com.github.ianellis.clean.Dependencies.testImplementations
+import com.github.ianellis.clean.plugins.CoverageCheckPlugin
+import com.github.ianellis.clean.plugins.CoverageCheckConfig
 
 plugins {
     id("java-library")
     id("kotlin")
     kotlin("kapt")
 }
+apply<CoverageCheckPlugin>()
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_7
@@ -20,4 +23,15 @@ dependencies {
     implementation(Dependencies.JsonParsing.Moshi.moshi)
     implementation(Dependencies.Kotlin.stdLib)
     implementation(Dependencies.Kotlin.coroutines)
+}
+
+configure<CoverageCheckConfig> {
+    testCommand = "test"
+    sourcePath = "src/main/java"
+    classesPath = "$buildDir/classes/kotlin/main"
+    excludes = listOf(
+        // Entities
+        "**/*Entity.class",
+        "**/*Entity$*.class"
+    )
 }

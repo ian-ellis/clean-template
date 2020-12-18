c5d2c29c4ba66d8a485d1267c85527aefed8c848
Ian Ellis <>
2020-12-18 15:47:19 +1100

    Adding coverage check plugin to :domain

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,9 +1,13 @@
 import com.github.ianellis.clean.Dependencies
 import com.github.ianellis.clean.Dependencies.testImplementations
+import com.github.ianellis.clean.plugins.CoverageCheckPlugin
+import com.github.ianellis.clean.plugins.CoverageCheckConfig
+
 plugins {
     id("java-library")
     id("kotlin")
 }
+apply<CoverageCheckPlugin>()
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_7
@@ -16,4 +20,10 @@ dependencies {
 
     testImplementations(Dependencies.Test.Unit.all)
     testImplementation(Dependencies.Test.Android.archCore)
+}
+
+configure<CoverageCheckConfig> {
+    testCommand = "test"
+    sourcePath = "src/main/java"
+    classesPath = "$buildDir/classes/kotlin/main"
 }
